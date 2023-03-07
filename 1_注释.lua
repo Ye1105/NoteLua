f6baf967fd36c5b9e2bc3de1e5119518c9477bae
15868707168@163.com <15868707168@163.com>
2023-03-07 16:29:43 +0800

    项目初始化

---
+++ 1_注释.lua
@@ -0,0 +1,17 @@
+--[[
+Author: 15868707168@163.com 15868707168@163.com
+Date: 2023-03-07 15:00:27
+LastEditors: 15868707168@163.com 15868707168@163.com
+LastEditTime: 2023-03-07 15:01:36
+FilePath: \LuaLesson\1.lua
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+--]]
+-- 单行注释
+-- print("单行");
+
+-- 多行注释
+--[[
+print("多行");
+print("多行");
+]]
+print("hello world !");
