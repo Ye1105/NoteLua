8833f7b9dbefc54a5414b9e7d08bd568e0d857b1
15868707168@163.com <15868707168@163.com>
2023-03-15 10:02:13 +0800

    对象自索引实现继承

---
+++ 13.3_面向对象自索引实现继承.lua
@@ -0,0 +1,43 @@
+--[[
+Author: 15868707168@163.com 15868707168@163.com
+Date: 2023-03-15 09:32:37
+LastEditors: 15868707168@163.com 15868707168@163.com
+LastEditTime: 2023-03-15 09:32:44
+FilePath: \LuaLesson\13.3_面向对象自索引实现继承.lua
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+--]]
+--[[
+    NOTE:自索引实现继承 典型的面向对象的写法，子类继承父类，并调用父类的方法
+
+]]
+father = {
+    a = 1,
+    b = 2
+}
+
+function father:faSay()
+    print("hello father a ：", self.a)
+end
+
+father:faSay()
+
+--father.__index = father --自索引
+
+son = {
+    c = 3,
+    d = 4
+}
+
+function son:sonSay()
+    print("hello son c :", self.c)
+end
+
+son:sonSay()
+
+print("++++++++++++++++++++++++++++++++")
+
+setmetatable(son, father) -- 加元表
+
+print(son.d)
+
+son:faSay() --典型的面向对象的写法，子类继承父类，并调用父类的方法
---
+++ 13.4_类的实例化.lua
@@ -0,0 +1,11 @@
+--[[
+Author: 15868707168@163.com 15868707168@163.com
+Date: 2023-03-15 10:00:32
+LastEditors: 15868707168@163.com 15868707168@163.com
+LastEditTime: 2023-03-15 10:01:48
+FilePath: \LuaLesson\13.4_类的实例化.lua
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+--]]
+--[[
+
+]]
